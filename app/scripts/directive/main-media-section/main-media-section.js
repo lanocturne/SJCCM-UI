6e13f30d758269b90377a5a4af7ab373a8540692
Stanislaus <kanye.s@gmail.com>
2015-03-08 12:46:55 -0700

    youtube vgvideo

--- app/scripts/directive/main-media-section/main-media-section.js
+++ app/scripts/directive/main-media-section/main-media-section.js
@@ -4,18 +4,29 @@
 'use strict';
 
 angular.module('sjccm.directive.main.mediaSection', [
-  "com.2fdevs.videogular"
+  "com.2fdevs.videogular",
+  "com.2fdevs.videogular.plugins.controls",
+  "com.2fdevs.videogular.plugins.buffering",
+  "com.2fdevs.videogular.plugins.overlayplay",
+  "info.vietnamcode.nampnq.videogular.plugins.youtube"
 ])
   .directive('mainMediaSection', function () {
     return {
       restrict: 'E',
       templateUrl: 'scripts/directive/main-media-section/main-media-section.html',
       controller: function ($scope) {
-        console.log('play')
-      },
-
-      link: function (scope) {
-
+        $scope.config = {
+          autoHide: false,
+          autoHideTime: 3000,
+          autoPlay: false,
+          sources: [{src:'http://youtu.be/XMgZYykQHLM'}],
+          loop: false,
+          preload: "auto",
+          transclude: true,
+          theme: {
+            url: "../bower_components/videogular-themes-default/videogular.min.css"
+          }
+        };
       }
     }
   });
