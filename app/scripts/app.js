58c4a61b173d08afd87979ccbe9c6e9b53387ef0
Stanislaus <kanye.s@gmail.com>
2015-03-12 15:18:09 -0700

    footer zixun

---
+++ app/scripts/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sjccm
+ * @description
+ * # sjccm
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sjccm', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ui.router',
+    'ngSanitize',
+    'ngTouch',
+    'sjccm.constants',
+    'sjccm.admin',
+    'sjccm.index',
+    'mgcrea.ngStrap',
+    'ui.bootstrap',
+    'angularMoment',
+    'ngMaterial'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('main',{
+        abstract: true,
+        views:{
+          main:{},
+          header:{
+            templateUrl:'views/header.html',
+            controller:'HeaderCtrl'
+          },
+          footer:{
+            templateUrl:'views/footer.html'
+          }
+        }
+      });
+  });
