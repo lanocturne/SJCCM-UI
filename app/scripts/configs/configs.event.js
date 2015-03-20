90c298b87fe73635bd35b0ed641c093e5159c2e5
Stanislaus <kanye.s@gmail.com>
2015-03-19 17:32:15 -0700

    SAYT

--- app/scripts/configs/configs.event.js
+++ app/scripts/configs/configs.event.js
@@ -2,32 +2,32 @@
 /**
  * Created by stan on 2/28/15.
  */
-angular.module('sjccm.event',['sjccm.event.list','sjccm.event.detail'])
-  .constant('EventStates',{
-    list:'events',
-    detail:'events.detail'
+angular.module('sjccm.event', ['sjccm.event.list', 'sjccm.event.detail'])
+  .constant('EventStates', {
+    list: 'events',
+    detail: 'events.detail'
   })
-  .config(function($stateProvider){
-    $stateProvider.state('events',{
-      url:'/events',
-      parent:'main',
-      views:{
-        'main@':{
-          templateUrl:'views/event/events.html',
+  .config(function ($stateProvider) {
+    $stateProvider.state('events', {
+      url: '/events',
+      parent: 'main',
+      views: {
+        'main@': {
+          templateUrl: 'views/event/events.html',
           controller: 'EventsCtrl'
         }
       }
     })
-      .state('events.detail',{
+      .state('events.detail', {
         url: '/:id',
-        views:{
-          'main@':{
-            templateUrl:'views/event/event.detail.html',
+        views: {
+          'main@': {
+            templateUrl: 'views/event/event.detail.html',
             controller: 'EventCtrl'
           }
         },
-        resolve:{
-          event:function(EventService,$stateParams){
+        resolve: {
+          event: function (EventService, $stateParams) {
             return EventService.get($stateParams.id);
           }
         }
